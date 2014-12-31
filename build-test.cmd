8e9ab9cd02000e9f024638c885f50d2b92bd679c
Anthony Sneed <tony@tonysneed.com>
2014-12-31 17:57:46 +0100

    Updated build.cmd to generate protobuf package.

--- build-test.cmd
+++ build-test.cmd
@@ -1,3 +1,3 @@
 set PackageVersion=1.0.0-alpha1
 rem pass 1 to debug, 0 to run without pausing
-build.cmd
+build.cmd 1
--- build.cmd
+++ build.cmd
@@ -11,18 +11,22 @@ set version="%PackageVersion%"
 REM Restore:
 if "%debug%"=="1" pause
 src\AspnetWebApi2Helpers\.nuget\nuget.exe install "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization\packages.config" -OutputDirectory "src\AspnetWebApi2Helpers\packages" -NonInteractive
+src\AspnetWebApi2Helpers\.nuget\nuget.exe install "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization.Protobuf\packages.config" -OutputDirectory "src\AspnetWebApi2Helpers\packages" -NonInteractive
 if "%debug%"=="1" pause
 if not "%errorlevel%"=="0" exit
 
 REM Build:
 if "%debug%"=="1" pause
 mkdir "releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization\portable-net45+wp80+win8+wpa81"
+mkdir "releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization.Protobuf\net45"
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization\AspnetWebApi2Helpers.Serialization.csproj" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization\portable-net45+wp80+win8+wpa81\msbuild.log /verbosity:normal /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization.Protobuf\AspnetWebApi2Helpers.Serialization.Protobuf.csproj" /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization.Protobuf\net45\msbuild.log /verbosity:normal /nr:false
 if "%debug%"=="1" pause
 if not "%errorlevel%"=="0" exit
 
 REM Package:
 if "%debug%"=="1" pause
 src\AspnetWebApi2Helpers\.nuget\nuget.exe pack "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization\AspnetWebApi2Helpers.Serialization.csproj" -symbols -o "releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization\portable-net45+wp80+win8+wpa81" -p Configuration=%config%;PackageVersion=%version%
+src\AspnetWebApi2Helpers\.nuget\nuget.exe pack "src\AspnetWebApi2Helpers\AspnetWebApi2Helpers.Serialization.Protobuf\AspnetWebApi2Helpers.Serialization.Protobuf.csproj" -symbols -o "releases\%PackageVersion%\AspnetWebApi2Helpers.Serialization.Protobuf\net45" -p Configuration=%config%;PackageVersion=%version%
 if "%debug%"=="1" pause
 if not "%errorlevel%"=="0" exit
